2f2702b0888ed5df066aa951a3aaa625f6f9eb0f
aashu <aashishrawte1@gmail.com>
2021-06-12 07:37:25 +0530

    add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitutde, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/forecast?access_key=ffb7dcd52fff2f437a53753a104c6936&query=' + latitutde + ',' + longitude 
+    const url = 'http://api.weatherstack.com/forecast?access_key=ffb7dcd52fff2f437a53753a104c6936&query=' + latitutde + ',' + longitude ; 
 
     request({ url, json: true}, (error, {body}) => {
         if (error) {
@@ -9,8 +9,8 @@ const forecast = (latitutde, longitude, callback) => {
         } else if (body.error) {
             callback('',undefined)
         } else {
-            callback(undefined, 'It is currently' + body.current.temperature + ' degree out. There is a ' + body.current.precip + ' % chance of rain'
-            )
+            console.log(body.current.data[0])
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degree out. It feels like " + body.current.feelslike + " degree out. The humidity is " + body.current.humidity + "%")
         }
     })
 }
